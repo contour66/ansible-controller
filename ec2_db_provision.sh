12faed5d49ca990687bc7188a69cdc8379d18fea
contour66 <zamir.thind@gmail.com>
2020-07-01 18:47:10 -0700

    ec2

--- ec2_db_provision.sh
+++ ec2_db_provision.sh
@@ -1,13 +1,3 @@
 #!/usr/bin/bash
-
-sudo yum install -y postgresql
-sudo yum install -y gcc
-sudo yum install -y python3-devel
-sudo yum install -y postgresql-devel
-sudo yum install -y jq # json parser
-sudo yum install -y git
-sudo pip3 install psycopg2
-sudo pip3 install boto3
-cd /home/ec2-user
-pip3 install --user boto3
-pip3 install --user psycopg2
+cd ~/python-image-gallery
+postgres -h
